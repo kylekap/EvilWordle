cc134b1c138a58871dc40993f9dbfef1475ea45d
kylekap <kylekaptbone@yahoo.com>
2022-01-13 18:15:37 -0500

    implementation of the loop. Need to troubleshoot the repeat letters issues

--- Project/core.py
+++ Project/core.py
@@ -3,6 +3,11 @@
 import requests
 import itertools
 import fnmatch
+import re
+import time
+import csv 
+
+
 import config
 
 
@@ -50,12 +55,16 @@ def guess_dict(guess):
     """    
     g_dict = {}
     for L in range(1,len(guess)+1):
-        for subset in itertools.permutations(guess+'?????',5): #? for wildcards in fnmatch. Need 5 in case there are no correct letters selected
+        for subset in itertools.permutations(guess+'?????',5): #? for wildcards. Need 5 in case there are no correct letters selected
             g_dict[''.join(subset)] = []
     return g_dict
 
 
-def check_permutation(perm_dict, possible_words):
+def removeDupWithoutOrder(str): 
+    return "".join(set(str)) 
+
+
+def check_permutation(perm_dict, possible_words,guess):
     """[summary]
 
     Args:
@@ -65,28 +74,65 @@ def check_permutation(perm_dict, possible_words):
     Returns:
         [type]: [description]
     """
-    #how to make faster?    
+    #how to make faster?
     for key in perm_dict:
+        except_letters = ''
+        for letter in guess:
+            if letter not in key:
+                except_letters=removeDupWithoutOrder(except_letters+letter)
+        expr = key.replace('?',f'[^{except_letters}]')
         for word in possible_words:
-            if fnmatch.fnmatch(word,key): #Currently including the other letters as a part of the wildcard... need to exclude.
-                #https://stackoverflow.com/questions/41471030/python-exclude-directory-with-fnmatch
-                perm_dict[key].append(word)
-            continue
-
+            if None != re.match(expr,word):
+               perm_dict[key].append(word)
+               
     return perm_dict
 
+
+def check_positional(actual,guess,included_letters):
+    val = {'match':'','others':included_letters}
+    for pos in range(0,len(guess)):
+        if actual[pos] == guess[pos]: #If it's in the right spot, make sure to include it & remove from wrong pos letters.
+            val['match'] = val.get('match','')+actual[pos]
+            val['others'] = val.get('others','').replace(actual[pos],'')
+        elif guess[pos] in actual: #if in the word but NOT right spot, list it
+            val['others']= removeDupWithoutOrder(val.get('others','')+guess[pos])
+            val['match']= val.get('match','')+'?'
+        else: #if not at all right spot, keep the spot blank
+            val['match']= val.get('match','')+'?'
+    return val
 
 def main():
     """[summary]
     """
+    guess_history = []
+    starttime = time.time()
     all_words = get_words()
     possible_words = all_words
-    guess = get_guess(possible_words)
-    a = check_permutation(guess_dict(guess),possible_words)
-    for k in sorted(a, key=lambda k: len(a[k]), reverse=False):
-        print(k,'With:',len(a.get(k)))
-    #Find the item with the highest value, that's where the word lies
-    #Then need to check if the guess has letters in the right positions or no
+
+    while True:
+        guess = get_guess(possible_words)
+        guess_history.append(guess)
+        included_letters = ''
+        result_set = check_permutation(guess_dict(guess),possible_words,guess)
+        max_key = max(result_set, key= lambda x: len(set(result_set[x])))
+        included_letters = max_key.replace('?','')
+        possible_words = result_set.get(max_key)
+
+        #Then need to check if the guess has letters in the right positions or no
+        return_prompt = check_positional(max_key,guess,included_letters)
+        a = return_prompt.get('match','?????')
+        b = return_prompt.get('others','')
+        c = len(result_set.get(max_key))
+        print(f'Current correct letters: {a}\nCurrent correct but non-ordered letters: {b}\nPossible words left: {c}')
+        print(result_set.get(max_key))
+        if c == 1:
+            print(f'Drat. You got it... the word was {a}')
+            break
+        else:
+            continue
+    
+    #End Loop
+    print(time.time()-starttime)
 
 if __name__ == '__main__':
     """[summary]
